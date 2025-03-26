1b962082be79e85f4eac523a4ca9dbc7a9a68067
Abhishek16OS <Abhishek16business@protonmail.com>
2025-03-26 10:02:32 +0530

    Lazy Loading Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import HookUseReducer from "./Component/HookUseReducer";
+import { useState } from "react";
+import { Suspense } from "react";
+import { lazy } from "react"
 
 
+const LazyLoading=lazy(()=>import('./Component/LazyLoading'));
 export default function App(){
+  const[load,setLoad]=useState(false);
+  function handleLoad(){
+    setLoad(true)
+  }
   return(<div>
-<HookUseReducer></HookUseReducer>
-   
+      <h1>Lazy Loading</h1>
+      {
+        load && <Suspense fallback={<p>Loading....</p>}>
+      <LazyLoading></LazyLoading>
+    </Suspense>
+      }
+   <button onClick={()=>handleLoad()} >Load User</button>
   </div>)
 }
---
+++ src/Component/LazyLoading.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function LazyLoading(){
+    return(<div>
+    <h1>Abhishek</h1>
+     
+    </div>)
+  }
+  
