373b79bcab96f58f60383b8f3b2c48ab082b8520
Abhishek16OS <Abhishek16business@protonmail.com>
2025-03-27 10:25:53 +0530

    useActionState Api revision again with changes

--- src/Component/HookUseActionState.jsx
+++ src/Component/HookUseActionState.jsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+import { useActionState } from "react";
+
+
 export default function HookUseActionState(){
+    
+  function handleForm(state,formData){
+
+    const userName=formData.get('name');
+    const age=formData.get('age');
+    const skill=formData.get('skill');
+   
+    return({...state,userName,age,skill});
+
+  }  
+
+
+  const [data,formAction,pending]=useActionState(handleForm,undefined);
+    
+
+    
+    
     return(<div>
       
+
+      <form action={formAction}  >
+
+      <input  placeholder="Enter user name " type="text" name="name"  />
+      <br />
+      <br />
+      <input placeholder="Enter user age " type="text" name="age"  />
+      <br />
+      <br />
+      <input  placeholder="Enter user skill " type="text" name="skill"  />
+      <br />
+      <br />
+      <button disabled={pending} type="submit"> {pending?'Submitting':'Submit'} </button>
+      <br />
+      <br />
+
+      </form>
+
+
+      {
+        data && <ul>
+          <li>{data.userName}</li>
+          <li>{data.skill}</li>
+          <li>{data.age}</li>
+        </ul>
+      }
+
     </div>)
   }
